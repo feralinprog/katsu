35075ed562e8db3a5a866de4487b4077561a0d89
Grant Posner <grant.posner@berkeley.edu>
2024-12-28 10:24:38 -0800

    Colored error output.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from parser import parser
 
+from termcolor import colored, cprint
+
 from error import ParseError, RunError
 from interpreter import eval, global_context
 from lexer import TokenStream, TokenType, get_all_tokens
@@ -17,20 +19,42 @@ def show_error(header_prefix: str, span: SourceSpan):
     print(f"{header_prefix} (at {span}):")
 
     context_lines = 2  # excluding error line itself
+    color_output = True
 
     text_lines = span.file.source.split("\n")
     for line, text in enumerate(text_lines):
-        if start.line - context_lines <= line <= end.line + context_lines:
-            print(f"| {text}")
-        if line == start.line:
-            end_col = end.column if end.line == start.line else len(text)
-            print("+ " + " " * start.column + "^" * (end_col - start.column))
-        elif start.line < line < end.line:
-            ws = len(text) - len(text.lstrip(" "))
-            print("+ " + " " * ws + "^" * (len(text) - ws))
-        elif line == end.line:
-            ws = len(text) - len(text.lstrip(" "))
-            print("+ " + " " * ws + "^" * (end.column - ws))
+        if color_output:
+            if not (start.line - context_lines <= line <= end.line + context_lines):
+                continue
+            if line == start.line:
+                end_col = end.column if end.line == start.line else len(text)
+                print(
+                    colored("+ ", "blue")
+                    + text[: start.column]
+                    + colored(text[start.column : end_col], "red")
+                    + text[end_col:]
+                )
+            elif start.line < line < end.line:
+                print(colored("+ ", "blue") + colored(text, "red"))
+            elif line == end.line:
+                print(
+                    colored("+ ", "blue") + colored(text[: end.column], "red") + text[end.column :]
+                )
+            else:
+                # Just context line.
+                print(colored("| ", "green") + text)
+        else:
+            if start.line - context_lines <= line <= end.line + context_lines:
+                print(f"| {text}")
+            if line == start.line:
+                end_col = end.column if end.line == start.line else len(text)
+                print("+ " + " " * start.column + "^" * (end_col - start.column))
+            elif start.line < line < end.line:
+                ws = len(text) - len(text.lstrip(" "))
+                print("+ " + " " * ws + "^" * (len(text) - ws))
+            elif line == end.line:
+                ws = len(text) - len(text.lstrip(" "))
+                print("+ " + " " * ws + "^" * (end.column - ws))
 
 
 try:
