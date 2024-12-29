97cf9be6647239e9c5e796ba70c0726277bc0ae0
Grant Posner <grant.posner@berkeley.edu>
2024-12-29 10:34:48 -0800

    Add quick syntax highlighted listing.

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ from termcolor import colored, cprint
 
 from error import ParseError, RunError
 from interpreter import NullValue, eval, global_context
-from lexer import TokenStream, TokenType, get_all_tokens
+from lexer import Token, TokenStream, TokenType, get_all_tokens
 from span import SourceFile, SourceSpan
 
 source_path = "./source.src"
@@ -71,6 +71,36 @@ def show_error(header_prefix: str, span: SourceSpan):
                 print("! " + " " * ws + "^" * (end.column - ws))
 
 
+def print_syntax_highlighted_tokens(tokens: list[Token]) -> None:
+    token_type_to_colorer = {
+        TokenType.EOF: (lambda t: colored(t, "light_grey")),
+        TokenType.ERROR: (lambda t: colored(t, "white", "on_red")),
+        TokenType.SEMICOLON: (lambda t: colored(t, "light_grey")),
+        TokenType.NEWLINE: (lambda t: colored(t, "light_grey")),
+        TokenType.WHITESPACE: (lambda t: colored(t, "light_grey")),
+        TokenType.COMMENT: (lambda t: colored(t, "magenta")),
+        TokenType.LPAREN: (lambda t: colored(t, "light_grey")),
+        TokenType.RPAREN: (lambda t: colored(t, "light_grey")),
+        TokenType.LCURLY: (lambda t: colored(t, "yellow")),
+        TokenType.RCURLY: (lambda t: colored(t, "yellow")),
+        TokenType.LSQUARE: (lambda t: colored(t, "blue")),
+        TokenType.RSQUARE: (lambda t: colored(t, "blue")),
+        TokenType.COMMA: (lambda t: colored(t, "light_grey")),
+        TokenType.NAME: (lambda t: colored(t, "green")),
+        TokenType.MESSAGE: (lambda t: colored(t, "cyan")),
+        TokenType.SYMBOL: (lambda t: colored(t, "light_magenta")),
+        TokenType.QUOTE: (lambda t: colored(t, "cyan")),
+        TokenType.OPERATOR: (lambda t: colored(t, "red")),
+        TokenType.NUMBER: (lambda t: colored(t, "light_grey")),
+        TokenType.STRING: (lambda t: colored(t, "light_blue")),
+    }
+    for token in tokens:
+        colorer = token_type_to_colorer.get(token._type, lambda t: t)
+        text = token.span.file.source[token.span.start.index : token.span.end.index]
+        print(colorer(text), end="")
+    print()
+
+
 # pretty-format into a string
 # TODO: line wrapping, indenting
 def pf(expr: Expr) -> str:
@@ -112,6 +142,10 @@ def pf(expr: Expr) -> str:
 try:
     file = SourceFile(source_path=source_path, source=source)
     tokens = get_all_tokens(file)
+
+    print("=" * 80)
+    print_syntax_highlighted_tokens(tokens)
+    print("=" * 80)
 
     stream = TokenStream(tokens)
     while stream.peek()._type != TokenType.EOF:
