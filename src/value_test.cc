192af3a80a44e27059914529e399fe2cd175ca92
Grant Posner <grant.posner@berkeley.edu>
2025-05-25 11:33:11 -0700

    Replace TEMPLATE_PRODUCT_TEST_CASE with explicitly macro-ized TEST_CASEs. This speeds up build process by ~3x.

--- src/value_test.cc
+++ src/value_test.cc
@@ -66,19 +66,6 @@ TEST_CASE("object tagging/untagging", "[value]")
     free(aligned);
 }
 
-// Curried pair type.
-template <typename A> struct Pair
-{
-    template <typename B> struct And
-    {
-        using T1 = A;
-        using T2 = B;
-    };
-};
-
-#define PAIR_LEFT(T) Pair<T>::And
-#define PAIR_RIGHT(T) T
-
 // Generic helper to create an example Value of any given tag.
 template <typename T> Value make();
 template <> Value make<int64_t>()
@@ -102,24 +89,39 @@ template <> Value make<Object*>()
     return Value::object(static_cast<Object*>(nullptr));
 }
 
-#define EACH_TYPE(F) (F(int64_t), F(bool), F(Null), F(Object*))
+#define EACH_TYPE_PAIR(F) \
+    F(int64_t, int64_t)   \
+    F(int64_t, bool)      \
+    F(int64_t, Null)      \
+    F(int64_t, Object*)   \
+    F(bool, int64_t)      \
+    F(bool, bool)         \
+    F(bool, Null)         \
+    F(bool, Object*)      \
+    F(Null, int64_t)      \
+    F(Null, bool)         \
+    F(Null, Null)         \
+    F(Null, Object*)      \
+    F(Object*, int64_t)   \
+    F(Object*, bool)      \
+    F(Object*, Null)      \
+    F(Object*, Object*)
 
-TEMPLATE_PRODUCT_TEST_CASE("tags keep track of underlying value type", "[value]",
-                           EACH_TYPE(PAIR_LEFT), EACH_TYPE(PAIR_RIGHT))
-{
-    using T1 = typename TestType::T1;
-    using T2 = typename TestType::T2;
-
-    Value v = make<T1>();
-
-    if (std::is_same<T1, T2>::value) {
-        CHECK_NOTHROW(v.value<T2>());
-    } else {
-        std::stringstream ss;
-        ss << "expected " << tag_str(make<T2>().tag());
-        CHECK_THROWS_MATCHES(v.value<T2>(), std::runtime_error, Message(ss.str()));
+#define TAG_TESTCASE(T1, T2)                                                            \
+    TEST_CASE("tags keep track of underlying value type - " #T1 " and " #T2, "[value]") \
+    {                                                                                   \
+        Value v = make<T1>();                                                           \
+                                                                                        \
+        if (std::is_same<T1, T2>::value) {                                              \
+            CHECK_NOTHROW(v.value<T2>());                                               \
+        } else {                                                                        \
+            std::stringstream ss;                                                       \
+            ss << "expected " << tag_str(make<T2>().tag());                             \
+            CHECK_THROWS_MATCHES(v.value<T2>(), std::runtime_error, Message(ss.str())); \
+        }                                                                               \
     }
-}
+
+EACH_TYPE_PAIR(TAG_TESTCASE)
 
 TEST_CASE("inline vs. non-inline tags", "[value]")
 {
@@ -150,39 +152,170 @@ TEST_CASE("object header distinguishes forwarding vs. reference types", "[object
     free(&obj);
 }
 
-#define EACH_OBJECT(F) \
-    (F(Ref),           \
-     F(Tuple),         \
-     F(Array),         \
-     F(Vector),        \
-     F(Module),        \
-     F(String),        \
-     F(Code),          \
-     F(Closure),       \
-     F(Method),        \
-     F(MultiMethod),   \
-     F(Type),          \
-     F(DataclassInstance))
+#define EACH_OBJECT_PAIR(F)           \
+    F(Ref, Ref)                       \
+    F(Ref, Tuple)                     \
+    F(Ref, Array)                     \
+    F(Ref, Vector)                    \
+    F(Ref, Module)                    \
+    F(Ref, String)                    \
+    F(Ref, Code)                      \
+    F(Ref, Closure)                   \
+    F(Ref, Method)                    \
+    F(Ref, MultiMethod)               \
+    F(Ref, Type)                      \
+    F(Ref, DataclassInstance)         \
+    F(Tuple, Ref)                     \
+    F(Tuple, Tuple)                   \
+    F(Tuple, Array)                   \
+    F(Tuple, Vector)                  \
+    F(Tuple, Module)                  \
+    F(Tuple, String)                  \
+    F(Tuple, Code)                    \
+    F(Tuple, Closure)                 \
+    F(Tuple, Method)                  \
+    F(Tuple, MultiMethod)             \
+    F(Tuple, Type)                    \
+    F(Tuple, DataclassInstance)       \
+    F(Array, Ref)                     \
+    F(Array, Tuple)                   \
+    F(Array, Array)                   \
+    F(Array, Vector)                  \
+    F(Array, Module)                  \
+    F(Array, String)                  \
+    F(Array, Code)                    \
+    F(Array, Closure)                 \
+    F(Array, Method)                  \
+    F(Array, MultiMethod)             \
+    F(Array, Type)                    \
+    F(Array, DataclassInstance)       \
+    F(Vector, Ref)                    \
+    F(Vector, Tuple)                  \
+    F(Vector, Array)                  \
+    F(Vector, Vector)                 \
+    F(Vector, Module)                 \
+    F(Vector, String)                 \
+    F(Vector, Code)                   \
+    F(Vector, Closure)                \
+    F(Vector, Method)                 \
+    F(Vector, MultiMethod)            \
+    F(Vector, Type)                   \
+    F(Vector, DataclassInstance)      \
+    F(Module, Ref)                    \
+    F(Module, Tuple)                  \
+    F(Module, Array)                  \
+    F(Module, Vector)                 \
+    F(Module, Module)                 \
+    F(Module, String)                 \
+    F(Module, Code)                   \
+    F(Module, Closure)                \
+    F(Module, Method)                 \
+    F(Module, MultiMethod)            \
+    F(Module, Type)                   \
+    F(Module, DataclassInstance)      \
+    F(String, Ref)                    \
+    F(String, Tuple)                  \
+    F(String, Array)                  \
+    F(String, Vector)                 \
+    F(String, Module)                 \
+    F(String, String)                 \
+    F(String, Code)                   \
+    F(String, Closure)                \
+    F(String, Method)                 \
+    F(String, MultiMethod)            \
+    F(String, Type)                   \
+    F(String, DataclassInstance)      \
+    F(Code, Ref)                      \
+    F(Code, Tuple)                    \
+    F(Code, Array)                    \
+    F(Code, Vector)                   \
+    F(Code, Module)                   \
+    F(Code, String)                   \
+    F(Code, Code)                     \
+    F(Code, Closure)                  \
+    F(Code, Method)                   \
+    F(Code, MultiMethod)              \
+    F(Code, Type)                     \
+    F(Code, DataclassInstance)        \
+    F(Closure, Ref)                   \
+    F(Closure, Tuple)                 \
+    F(Closure, Array)                 \
+    F(Closure, Vector)                \
+    F(Closure, Module)                \
+    F(Closure, String)                \
+    F(Closure, Code)                  \
+    F(Closure, Closure)               \
+    F(Closure, Method)                \
+    F(Closure, MultiMethod)           \
+    F(Closure, Type)                  \
+    F(Closure, DataclassInstance)     \
+    F(Method, Ref)                    \
+    F(Method, Tuple)                  \
+    F(Method, Array)                  \
+    F(Method, Vector)                 \
+    F(Method, Module)                 \
+    F(Method, String)                 \
+    F(Method, Code)                   \
+    F(Method, Closure)                \
+    F(Method, Method)                 \
+    F(Method, MultiMethod)            \
+    F(Method, Type)                   \
+    F(Method, DataclassInstance)      \
+    F(MultiMethod, Ref)               \
+    F(MultiMethod, Tuple)             \
+    F(MultiMethod, Array)             \
+    F(MultiMethod, Vector)            \
+    F(MultiMethod, Module)            \
+    F(MultiMethod, String)            \
+    F(MultiMethod, Code)              \
+    F(MultiMethod, Closure)           \
+    F(MultiMethod, Method)            \
+    F(MultiMethod, MultiMethod)       \
+    F(MultiMethod, Type)              \
+    F(MultiMethod, DataclassInstance) \
+    F(Type, Ref)                      \
+    F(Type, Tuple)                    \
+    F(Type, Array)                    \
+    F(Type, Vector)                   \
+    F(Type, Module)                   \
+    F(Type, String)                   \
+    F(Type, Code)                     \
+    F(Type, Closure)                  \
+    F(Type, Method)                   \
+    F(Type, MultiMethod)              \
+    F(Type, Type)                     \
+    F(Type, DataclassInstance)        \
+    F(DataclassInstance, Ref)         \
+    F(DataclassInstance, Tuple)       \
+    F(DataclassInstance, Array)       \
+    F(DataclassInstance, Vector)      \
+    F(DataclassInstance, Module)      \
+    F(DataclassInstance, String)      \
+    F(DataclassInstance, Code)        \
+    F(DataclassInstance, Closure)     \
+    F(DataclassInstance, Method)      \
+    F(DataclassInstance, MultiMethod) \
+    F(DataclassInstance, Type)        \
+    F(DataclassInstance, DataclassInstance)
 
-TEMPLATE_PRODUCT_TEST_CASE("object() helper checks object tags", "[object]", EACH_OBJECT(PAIR_LEFT),
-                           EACH_OBJECT(PAIR_RIGHT))
-{
-    using T1 = typename TestType::T1;
-    using T2 = typename TestType::T2;
-
-    Object& obj = *reinterpret_cast<Object*>(malloc(sizeof(Object)));
-    REQUIRE_NOTHROW(obj.set_object(T1::CLASS_TAG));
-
-    if (std::is_same<T1, T2>::value) {
-        CHECK(obj.object<T2*>() == reinterpret_cast<T2*>(&obj));
-    } else {
-        std::stringstream ss;
-        ss << "expected " << object_tag_str(T2::CLASS_TAG);
-        CHECK_THROWS_MATCHES(obj.object<T2*>(), std::runtime_error, Message(ss.str()));
+#define OBJECT_TAG_TESTCASE(T1, T2)                                                         \
+    TEST_CASE("object() helper checks object tags - " #T1 " and " #T2, "[object]")          \
+    {                                                                                       \
+        Object& obj = *reinterpret_cast<Object*>(malloc(sizeof(Object)));                   \
+        REQUIRE_NOTHROW(obj.set_object(T1::CLASS_TAG));                                     \
+                                                                                            \
+        if (std::is_same<T1, T2>::value) {                                                  \
+            CHECK(obj.object<T2*>() == reinterpret_cast<T2*>(&obj));                        \
+        } else {                                                                            \
+            std::stringstream ss;                                                           \
+            ss << "expected " << object_tag_str(T2::CLASS_TAG);                             \
+            CHECK_THROWS_MATCHES(obj.object<T2*>(), std::runtime_error, Message(ss.str())); \
+        }                                                                                   \
+                                                                                            \
+        free(&obj);                                                                         \
     }
 
-    free(&obj);
-}
+EACH_OBJECT_PAIR(OBJECT_TAG_TESTCASE)
 
 // TODO: test functions of Ref
 // TODO: test functions of Tuple
