cbecf9eada3384740506d8cfacbafa6d9cec2b97
Grant Posner <grant.posner@berkeley.edu>
2025-05-25 21:48:19 -0700

    Add initial value_utils tests.

--- src/value_utils_test.cc
+++ src/value_utils_test.cc
@@ -1,1 +1,26 @@
-// TODO
+#include <catch2/catch_test_macros.hpp>
+
+#include "value_utils.h"
+
+using namespace Katsu;
+
+TEST_CASE("make_ref - roots", "[value-utils]")
+{
+    GC gc(1024 * 1024);
+
+    String* string = make_string(gc, "pointee");
+    Root r_string(gc, Value::object(string));
+    Ref* ref = make_ref(gc, r_string);
+    CHECK(string_eq(ref->v_ref.obj_string(), "pointee"));
+}
+
+TEST_CASE("make_ref - values", "[value-utils]")
+{
+    GC gc(1024 * 1024);
+
+    String* string = make_string(gc, "pointee");
+    Ref* ref = make_ref(gc, Value::object(string));
+    CHECK(string_eq(ref->v_ref.obj_string(), "pointee"));
+}
+
+// TODO: test the rest
